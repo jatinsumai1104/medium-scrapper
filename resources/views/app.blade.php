f683a659413f39cfca7a8c3ed9e9e0fc2ebd6518
Jatin Sumai <43413309+jatinsumai1104@users.noreply.github.com>
2020-08-30 15:38:48 +0530

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,13 +9,13 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="https://medium-scrapper-jatin.herokuapp.com/css/style.css" rel="stylesheet">
 
 </head>
 <body>
 <div class="flex-center position-ref full-height">
     <div id="root"></div>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://medium-scrapper-jatin.herokuapp.com/js/app.js" defer></script>
 </div>
 </body>
 </html>
